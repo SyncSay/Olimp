a6d97983cdc040bccbfa244b599e64ccc82d593d
SyncSay <syroeginvictor@gmail.com>
2026-06-20 13:40:55 +0300

    Initial commit  Changes to be committed: 	new file:   .gitignore 	new file:   CMakeLists.txt 	new file:   main.c 	new file:   src/core/core.c 	new file:   src/core/core.h

---
+++ src/core/core.c
@@ -0,0 +1,4 @@
+#include "core.h"
+#include <stdalign.h>
+#include <stdint.h>
+#include <stdbool.h>
---
+++ src/core/core.h
@@ -0,0 +1,68 @@
+#pragma once
+
+/*------------------------------------------------------
+* Here I'm implementing:
+* a blob key engine for the database
+* writing/reading WAL logs
+* storage structures
+* a stack of blob addresses (shadow stack)
+* auxiliary utilities
+
+P.S.
+Blob (from the English "Binary Large Object")
+is a term denoting an array
+of unstructured binary data stored as a single unit.
+--------------------------------------------------------*/
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+#define STANDART_BLOB 32768
+#define STANDART_DATASIZE (16 * 1024 ** 2)
+#define STANDART_WALSIZE (10 * 1024 * 1024 * 8)
+
+
+typedef struct {
+  uint8_t data[16777216]; // = STANDART_DATAASIZE
+  uint64_t used;
+} BStore; // Blob Store
+
+typedef struct {
+    uint64_t sddr; //offset blob
+    uint64_t size;
+} BEntry;
+
+typedef struct {
+  uint64_t index; // name blob
+    uint64_t old_addres;
+    uint64_t old_size; // needed to delete and rewrite the WAL log
+    uint64_t new_addres;
+    uint64_t new_size;
+    bool is_old; // was the blob written
+} WALEntry;
+
+
+typedef struct {
+    BStore store;
+    BEntry entries[STANDART_BLOB];
+    uint_fast64_t count;
+    WALEntry wal[STANDART_WALSIZE / sizeof(WALEntry)];
+
+    int data_fd;
+    int wal_fd;
+    int auto_save_sec;
+} Core;
+
+//-- API --
+
+void core_init(Core *core, const char *data_path, const char *wal_path);
+void core_close(Core *core);
+int core_put(Core *core, const void *data, uint64_t size); // retern blobs index
+void *core_get(Core *core, uint64_t idx,
+               uint64_t *size); // retern blobs pointer
+void core_update(Core *core, uint64_t idx, const void *data, uint64_t size);
+void core_delete(Core *core, uint64_t idx);
+void core_save(Core *core);
+void core_auto_save(Core *core, int sec);
+bool core_upload(Core *core, const char* main_wal, const char* sub_wal); // Load another WAL into the main one
